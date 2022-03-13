6d9c2b905eec1c9245fa4c95b55f8e2081018abc
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-03-13 13:51:35 +0600

    updated

--- FaceMeshModule.py
+++ FaceMeshModule.py
@@ -1,9 +1,10 @@
-import cv2
-import mediapipe as mp
-import time
+import cv2;
+import mediapipe as mp;
+import time;
 
 
 count = 0;
+face_orientation = 'front'
 
 class FaceMeshDetector():
 
@@ -14,34 +15,34 @@ class FaceMeshDetector():
                min_detection_confidence=0.5,
                min_tracking_confidence=0.5):
 
-        self.staticMode = static_image_mode
-        self.maxFaces = max_num_faces
-        self.refine_landmarks = refine_landmarks
-        self.minDetectionCon = min_detection_confidence
-        self.minTrackCon = min_tracking_confidence
+        self.staticMode = static_image_mode;
+        self.maxFaces = max_num_faces;
+        self.refine_landmarks = refine_landmarks;
+        self.minDetectionCon = min_detection_confidence;
+        self.minTrackCon = min_tracking_confidence;
 
-        self.mpDraw = mp.solutions.drawing_utils
-        self.mpFaceMesh = mp.solutions.face_mesh
+        self.mpDraw = mp.solutions.drawing_utils;
+        self.mpFaceMesh = mp.solutions.face_mesh;
         self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFaces, self.refine_landmarks,
-                                                 self.minDetectionCon, self.minTrackCon)
-        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+                                                 self.minDetectionCon, self.minTrackCon);
+        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2);
 
     def findFaceMesh(self, img, draw=True):
-        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.faceMesh.process(self.imgRGB)
-        faces = []
+        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB);
+        self.results = self.faceMesh.process(self.imgRGB);
+        faces = [];
         if self.results.multi_face_landmarks:
             for faceLms in self.results.multi_face_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS,
-                                           self.drawSpec, self.drawSpec)
+                                           self.drawSpec, self.drawSpec);
                 # face = []
                 for id,lm in enumerate(faceLms.landmark):
                     #print(lm)
-                    ih, iw, ic = img.shape
-                    x,y = int(lm.x*iw), int(lm.y*ih)
+                    ih, iw, ic = img.shape;
+                    x,y = int(lm.x*iw), int(lm.y*ih);
                     cv2.putText(img, str(id), (x, y), cv2.FONT_HERSHEY_PLAIN,
-                               0.7, (0, 255, 0), 1)
+                               0.7, (0, 255, 0), 1);
 
                     
                     # face.append([x,y])
@@ -49,7 +50,10 @@ class FaceMeshDetector():
         return img
       
     def find_Orientation(self, img, draw=True):
+      
       global count;
+      global face_orientation;
+
       if self.results.multi_face_landmarks:
           for faceLms in self.results.multi_face_landmarks:
             # print('right',faceLms.landmark[323].x)
@@ -58,23 +62,31 @@ class FaceMeshDetector():
             
             if faceLms.landmark[1].x >= faceLms.landmark[323].x:
               count += 1;
-              print("Right ", count);
+              face_orientation = "right";
+              # print("Right ", count);
               
             elif faceLms.landmark[1].x <= faceLms.landmark[93].x:
               count += 1;
-              print("Left ", count);
+              face_orientation = "left";
+              # print("Left ", count);
             
             elif faceLms.landmark[175].y >= faceLms.landmark[152].y:
               count += 1;
-              print("Down ", count);
+              face_orientation = "down";
+              # print("Down ", count);
             
             elif faceLms.landmark[134].y <= faceLms.landmark[127].y:
               count += 1;
-              print("Up ", count);
+              face_orientation = "up";
+              # print("Up ", count);
             
             elif faceLms.landmark[14].y - faceLms.landmark[13].y >= 0.015:
               count += 1;
-              print("Open ", count);
+              face_orientation = "open";
+              # print("Open ", count);
+            
+            else:
+              face_orientation = "font"
               
             
               
@@ -83,28 +95,28 @@ class FaceMeshDetector():
             # print('open', faceLms.landmark[159].y)
             # print('close', faceLms.landmark[145].y)
             
-      return img;
+      return img, face_orientation;
               
 
 
 def main():
-    cap = cv2.VideoCapture(0)
-    pTime = 0
-    detector = FaceMeshDetector(max_num_faces=1)
+    cap = cv2.VideoCapture(0);
+    pTime = 0;
+    detector = FaceMeshDetector(max_num_faces=1);
     
     while True:
         success, img = cap.read();
         # img = cv2.flip(img,1)
-        img, face = detector.findFaceMesh(img)
+        img, face = detector.findFaceMesh(img);
         # if len(face)!= 0:
         #     # print(face[0])
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+        cTime = time.time();
+        fps = 1 / (cTime - pTime);
+        pTime = cTime;
 
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
+        cv2.imshow("Image", img);
+        cv2.waitKey(1);
 
 
 if __name__ == "__main__":
-    main()
+    main();
---
+++ face_question.py
@@ -0,0 +1,153 @@
+import random
+import cv2
+import time
+
+
+question_bank = [
+    "right",
+    "left",
+    "up",
+    "down",
+    "open"             
+                 ];
+
+
+countdown = 0;
+status = None;
+counter_start = time.time();
+question_index = random.randint(0,4);
+question = question_bank[question_index];
+final_result_for_all_qstn = [];
+buffer_result_for_single_qstn = [];
+
+# Generating new question
+def generate_qstn(img):
+    global question_index;
+    global countdown;
+    global question;
+    global counter_start;
+    current_time = time.time()
+    time_counter = generate_timer(current_time);
+    
+    if counter_start + 3 < current_time:
+        
+        # calculate previous question answer
+        if status == None:
+            # print("prev_qstn", question)
+            calculate_result(question);
+        
+        new_question_index = random.randint(0,4);
+        if question_index == new_question_index:
+            if question_index == 0:
+                new_question_index = question_index + 1;
+            else:
+                new_question_index = question_index - 1;
+                
+        counter_start = current_time;
+        countdown = 0;
+        question_index = new_question_index;
+        question = question_bank[question_index];
+        
+        
+    
+    # check status
+    if status != None:
+        display_result(img);
+       
+    else:
+        display_question(img, question, time_counter);
+        return question;
+
+
+#Edit image frame with question
+def display_question(img, question, timer):
+    
+    text = str(question);
+    cv2.rectangle(img, (100, 10), (450, 50), (0, 255, 0), cv2.FILLED);
+    cv2.putText(img,f'Timer: {timer}',(510,35),cv2.FONT_HERSHEY_COMPLEX,0.7,(0, 65, 255),1);
+    cv2.putText(img,text,(110,40),cv2.FONT_HERSHEY_COMPLEX,1,(255, 0, 0),2);
+    return img;
+
+# Edit image frame with result
+def display_result(img):
+    global status;
+    if status == "Verified":
+        bg = (106, 176, 76);
+        txt_position = (240,40)
+    else:
+        bg = (0, 0, 205);
+        txt_position = (220,40);
+    cv2.rectangle(img, (170, 10), (440, 50), bg, cv2.FILLED);
+    cv2.putText(img, status,txt_position,cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2);
+    return img;
+    
+
+# Check match for every buffering question
+def match_q_a(given_face_orientation):
+    
+    global buffer_result_for_single_qstn;
+    global counter_start;
+    buffer_result_for_single_qstn.append(given_face_orientation);
+    
+
+# Generating countdown for each question
+def generate_timer(current_time):
+    
+    global countdown;
+    global counter_start;
+    
+    if  counter_start + 1 < current_time:
+        countdown += 1;
+        return countdown;
+    else:
+        return countdown;
+
+
+# Result Calculation
+def calculate_result(question):
+    
+    global final_result_for_all_qstn;
+    global buffer_result_for_single_qstn;
+    global question_bank;
+    
+    
+        
+    if buffer_result_for_single_qstn.count(question):
+        false_value_count = 0
+        for qstn in question_bank:
+            if qstn == question or qstn == 'font':
+                continue;
+            else:
+                if buffer_result_for_single_qstn.count(qstn):
+                    false_value_count += 1;
+        if false_value_count:
+            final_result_for_all_qstn.append(0);
+            print("false")
+        else:
+            final_result_for_all_qstn.append(1);
+            print("true");
+            
+    else:
+        final_result_for_all_qstn.append(0);
+        print("false");
+    
+    buffer_result_for_single_qstn.clear();
+    
+    if len(final_result_for_all_qstn) == 4:
+        make_decision(final_result_for_all_qstn);
+        final_result_for_all_qstn.clear();
+        print(final_result_for_all_qstn);
+
+
+# Make a decision for varified or not
+def make_decision(final_result_for_all_qstn):
+    
+    global status;
+    rignt_ans_count = final_result_for_all_qstn.count(1);
+    
+    if(rignt_ans_count >= 3):
+        status = "Verified"
+        print("Passed")
+    else:
+        status = "Unverified"
+        print("failed")
